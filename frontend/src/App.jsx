d8c50c901c621ec9fa6534b8e254f1998360e4a0
Supun Piyumal <supunthennakoon27@gmail.com>
2026-05-15 19:36:07 +0530

    implement routing for Home and Service components in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Service from './pages/Service'
 
 function App() {
-  return <Home />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Service />} />
+    </Routes>
+  )
 }
 
 export default App
