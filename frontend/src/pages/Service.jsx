b196176420067d050bde050a4d3ea56b538b5f96
Supun Piyumal <supunthennakoon27@gmail.com>
2026-05-18 09:53:42 +0530

    adjust indentation of Header component in Service page

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -4,7 +4,7 @@ import ServicesSection from '../components/ServicesSection'
 const Service = () => {
   return (
     <div className="min-h-screen bg-[#f5f7fb] px-[clamp(20px,6vw,72px)] pb-20 pt-7 text-[#0f172a]">
-        <Header />
+      <Header />
       <div className="mt-10">
         <ServicesSection withContainer={false} />
       </div>
