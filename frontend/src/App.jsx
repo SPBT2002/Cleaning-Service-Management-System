c329b87f9973c27edf3d7c7dda9c5df1c2e34f59
Supun Piyumal <supunthennakoon27@gmail.com>
2026-05-22 21:31:02 +0530

    add client routes for Gallery, Review, and Contact pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Service from './pages/Service'
+import Home from './pages/client/Home'
+import Gallery from './pages/client/Gallery'
+import Service from './pages/client/Service'
+import Review from './pages/client/Review'
+import Contact from './pages/client/Contact'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<Service />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/reviews" element={<Review />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   )
 }
