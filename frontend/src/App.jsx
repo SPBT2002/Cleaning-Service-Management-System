1f080662872740ec7c4214f347c1892e03e9567b
Supun Piyumal <supunthennakoon27@gmail.com>
2026-05-25 13:19:08 +0530

    add admin routes for dashboard, bookings, services, and gallery

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import Review from './pages/client/Review'
 import Contact from './pages/client/Contact'
 import SignIn from './pages/login-pages/SignIn'
 import SignUp from './pages/login-pages/SignUp'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import AdminBookings from './pages/admin/AdminBookings'
+import AdminServices from './pages/admin/AdminServices'
+import AdminGallery from './pages/admin/AdminGallery'
 
 function App() {
   return (
@@ -17,6 +21,10 @@ function App() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin/bookings" element={<AdminBookings />} />
+      <Route path="/admin/services" element={<AdminServices />} />
+      <Route path="/admin/gallery" element={<AdminGallery />} />
     </Routes>
   )
 }
